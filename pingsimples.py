24168567abcb70521bbf8096cba190f41d6cb14e
Arnaldo Filho <arnalldofilho@gmail.com>
2022-03-13 22:25:36 -0300

    corrigindo comando ping -c *

--- pingsimples.py
+++ pingsimples.py
@@ -4,5 +4,5 @@ print("#" * 60)
 
 ip_ou_host = input("Digite o IP ou host a ser verificado: ") # criando uma variável que recebe do usuário o IP.
 print("_" * 60)
-os.system('ping -n 6 {}'.format(ip_ou_host)) # chamando o método system da bliblioteca os chamando o comando ping com 6 pacotes virão do IP {}
+os.system('ping -c 6 {}'.format(ip_ou_host)) # chamando o método system da bliblioteca os chamando o comando ping com 6 pacotes virão do IP {}
 print("_" * 60)
